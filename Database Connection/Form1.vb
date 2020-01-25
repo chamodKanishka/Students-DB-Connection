fc5a39537fa7d84a1681add6914864ef2e7d4b78
ChamodKanishka <chamodkanishka77@gmail.com>
2020-01-25 15:08:17 +0530

    update data by using text fileds

---
+++ Database Connection/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Conn As New SqlConnection
+        Dim cmd As New SqlCommand
+
+        Conn.ConnectionString = "Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Chathuranga\AppData\Local\Temporary Projects\WindowsApplication1\StudentBase.mdf;Integrated Security=True;User Instance=True"
+        Conn.Open()
+        cmd.Connection = Conn
+        cmd.CommandText = "INSERT INTO StudentInfo(Name,Age) Values(@Name,@Age);"
+        cmd.Parameters.AddWithValue("Name", TextBoxName.Text)
+        cmd.Parameters.AddWithValue("Age", TextBoxAge.Text)
+        cmd.ExecuteNonQuery()
+        Conn.Close()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBoxAge.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
